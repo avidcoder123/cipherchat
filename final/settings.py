790cfb07885fb514102ab6e2ab0c883bce266c0e
thomcatmeow <60586376+thomcatmeow@users.noreply.github.com>
2020-10-13 13:31:40 -0500

    Update settings.py

--- final/settings.py
+++ final/settings.py
@@ -86,10 +86,14 @@ CHANNEL_LAYERS = {
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+     'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("dbname"),
+        'USER': os.environ.get("dbuser"),
+        'PASSWORD': os.environ.get("dbpass"),
+        'HOST': os.environ.get("dbhost"),
+        'PORT': os.environ.get("dbport"),
 }
 AUTH_USER_MODEL = "capstone.User"
 
