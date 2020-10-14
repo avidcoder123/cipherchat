731d7cd3f4793653be263d118ff40235587f0a5e
thomcatmeow <60586376+thomcatmeow@users.noreply.github.com>
2020-10-13 21:20:43 -0500

    Update util.py

--- capstone/util.py
+++ capstone/util.py
@@ -1,6 +1,8 @@
 from django.db import close_old_connections
+from functools import wraps
 
 def manageconnections(func):
+    @wraps(func)
     def wrapper(*args,**kwargs):
         close_old_connections()
         func(*args,**kwargs)
