a7c698c475f6cd4b41fe3e31666b07fce9c6f8b4
thomcatmeow <60586376+thomcatmeow@users.noreply.github.com>
2020-10-13 20:17:26 -0500

    Update settings for redis

--- final/settings.py
+++ final/settings.py
@@ -77,7 +77,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [('ec2-3-212-177-190.compute-1.amazonaws.com', 10429)],
         },
     },
 }
