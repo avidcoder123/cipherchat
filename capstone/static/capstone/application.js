d2b61a633a32536e6820c6b19f611e3255098136
thomcatmeow <60586376+thomcatmeow@users.noreply.github.com>
2020-10-15 15:16:13 -0500

    Update application.js

--- capstone/static/capstone/application.js
+++ capstone/static/capstone/application.js
@@ -75,27 +75,6 @@ document.addEventListener('DOMContentLoaded', () => {
             window.scrollTo(0, document.body.scrollHeight || document.documentElement.scrollHeight);
             let data = JSON.parse(e.data);
             data = JSON.parse(data.message);
-            if(!document.hasFocus()){
-                //developer.mozilla.org
-                  if (!("Notification" in window)) {
-                    alert("This browser does not support desktop notification");
-                    }
-                  // Let's check whether notification permissions have already been granted
-                  else if (Notification.permission === "granted") {
-                    // If it's okay let's create a notification
-                    var notification = new Notification(`${data.sender} says: ${cryptico.decrypt(data.body,privatekey).plaintext}`);
-                  }
-
-                  // Otherwise, we need to ask the user for permission
-                  else if (Notification.permission !== "denied") {
-                    Notification.requestPermission().then(function (permission) {
-                      // If the user accepts, let's create a notification
-                      if (permission === "granted") {
-                        var notification = new Notification(`${data.sender} says: ${cryptico.decrypt(data.body,privatekey).plaintext}`);
-                      }
-                    });
-                }         
-            }
             if(data.recipient == currentuser) {
                 let message=`<div class="alert alert-${currentuser === data.sender ? "success":"dark"}" role="alert">\
                 <a href="/profile/${data.sender}">@${data.sender}</a><hr>\
